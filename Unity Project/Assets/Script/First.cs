ad0e62423bebd6fb2e5f5b3cf508a55dfc687fb1
Reduichi <q2339198@gmail.com>
2019-11-12 21:31:34 +0800

    添加腳本

---
+++ Unity Project/Assets/Script/First.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine; //引用Unity API
+
+//類別 類別名稱
+public class First : MonoBehaviour 
+{
+    // 定義欄位 Field(宣告變數)
+    // 語法 :
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
+    // 私人 private  - 隱藏(預設)
+    // 公開 public   - 顯示
+ 
+    public float floor;
+    public bool bird;
+    public int GameManager;
+
+}
