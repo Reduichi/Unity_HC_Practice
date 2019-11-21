8496edc2cb50dea8b4ab87a6aad17431cc762da3
Reduichi <q2339198@gmail.com>
2019-11-21 21:33:49 +0800

    完成水管的事件

--- Unity Project/Assets/Script/GameManager.cs
+++ Unity Project/Assets/Script/GameManager.cs
@@ -6,6 +6,27 @@ public class GameManager : MonoBehaviour
     public int score;
     [Header("最高分數")]
     public int scoreHeight;
+    [Header("水管")]
+    public GameObject pipe; // GameObject 可以存放預置物以及場景上的物件
+
+    /// <summary>
+    /// 生成水管功能
+    /// </summary>
+    public void SpawnPipe()
+    {
+        // 因為有繼承此類別才可簡寫
+        // Object.Instantiate(pipe);
+
+        // 浮點數 = 隨機.靜態方法(最小，最大)
+        float y = Random.Range(-0.6f , 0.56f);
+
+        // 三維向量 Vector3 - x,y,z
+        Vector3 pos = new Vector3(4.5f , y , 0);
+
+        // 四元數 Quaternion - x,y,z,w
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+        Instantiate(pipe, pos, rot);
+    }
 
     /// <summary>
     /// 加分功能
@@ -17,10 +38,25 @@ public class GameManager : MonoBehaviour
     }
 
     /// <summary>
+    /// 設定最高分數
+    /// </summary>
+    private void SetHeightScore()
+    {
+
+    }
+
+    /// <summary>
     /// 遊戲結束
     /// </summary>
     public void GameOver()
     {
 
+    }
+
+    private void Start()
+    {
+        // SpawnPipe();
+        // 重複延遲物件生產時間
+        InvokeRepeating("SpawnPipe", 0, 1.5f);
     }
 }
